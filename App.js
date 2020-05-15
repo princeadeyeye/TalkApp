51711ca91ba2750ff680e780952b5b1c3568bb08
teezyfortune <muizadeyeye@gmail.com>
2020-05-15 13:14:29 +0100

    signup-ui

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import firebase from 'firebase';
 import firebaseConfig from './config'
 import 'firebase/firestore';
 import Login from './src/components/screens/Login'
+import Signup from './src/components/screens/Signup'
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login />
+      <Signup />
     </View>
   );
 }
